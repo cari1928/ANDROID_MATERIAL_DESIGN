4dc52e86f418b648d90e882ac298e232813ca721
Cari Santana <radogan1928@hotmail.com>
2017-07-16 20:45:19 -0500

    Adding Toolbar

--- app/src/main/java/com/example/radog/materialtest/MainActivity.java
+++ app/src/main/java/com/example/radog/materialtest/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.radog.materialtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.app_bar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //to use our customized action bar
+        setSupportActionBar(toolbar);
     }
 }
